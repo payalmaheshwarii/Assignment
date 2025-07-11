dd6d40eaf7f414a1cdf9d6f95dbc6ac315c7850b
payalmaheshwarii <payalmaheshwar2610@gmail.com>
2025-07-12 00:10:57 +0530

    Phase 3

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+const customDelimiters = ['-', '..', 'to', '~']
+
 function expandRanges(input) {
 
   const result = [];
   const ranges = input.split(',').map(part => part.trim()).filter(Boolean);
 
+
   for (const part of ranges) {
-    if (part.includes('-')) {
-      const [start, end] = part.split('-').map(no => parseInt(no));
+    const currentDelimiter = customDelimiters.find(delimiter => part.includes(delimiter));
+    console.log(currentDelimiter);
+
+    if (part.includes(currentDelimiter)) {
+      const [start, end] = part.split(currentDelimiter).map(no => parseInt(no));
       for (let i = start; i <= end; i++) {
         result.push(i);
       }
--- test.js
+++ test.js
@@ -7,3 +7,6 @@ console.log(expandRanges("1-2,4")); // [1, 2, 4]
 
 //stage 2
 console.log(expandRanges(" , 1-3 , ,5 ")); // [1, 2, 3, 5]
+
+//stage 3
+console.log(expandRanges(" , 1..3 , ,5-7, 10~12 ")); // [1, 2, 3, 5, 6, 7, 10, 11, 12]
